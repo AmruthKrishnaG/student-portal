ef37dbdfd8289d2306e336b84e315e67e7c9b795
Amruth Krishna G <amruthkg@gmail.com>
2025-08-18 10:38:02 +0530

    feat: Basic form validations done

--- src/app/components/student-details-capture-reactive/student-details-capture-reactive.ts
+++ src/app/components/student-details-capture-reactive/student-details-capture-reactive.ts
@@ -1,16 +1,47 @@
-import { Component, output } from '@angular/core';
+import { Component, inject, OnInit, output } from '@angular/core';
 import { Student } from '../../models';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+export function rangeValidator(formControl: FormControl) {
+  const marks = formControl.value;
+  if (marks < 0 || marks > 100) {
+    return {
+      range: true,
+    };
+  }
+  return null;
+}
 
 @Component({
   selector: 'app-student-details-capture-reactive',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './student-details-capture-reactive.html',
   styleUrl: './student-details-capture-reactive.scss',
 })
-export class StudentDetailsCaptureReactive {
+export class StudentDetailsCaptureReactive implements OnInit {
   studentDetailsSubmit = output<Student>();
   removeThisArray: number[] = [];
-  onSubmit(studentDetails: Student) {
-    this.studentDetailsSubmit.emit(studentDetails);
+
+  private fb = inject(FormBuilder);
+  studentForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.studentForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      gender: ['', Validators.required],
+      group: ['', Validators.required],
+      marks: [0, [Validators.required, rangeValidator]],
+    });
   }
+
+  // onSubmit(studentDetails: Student) {
+  //   this.studentDetailsSubmit.emit(studentDetails);
+  // }
 }
